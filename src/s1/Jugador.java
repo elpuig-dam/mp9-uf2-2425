3e89bcf85bb3c7c852d9e1fc9f39fa8c055e0645
jherna <jherna76@elpuig.xeill.net>
2024-09-28 16:43:06 +0200

    update exercici jugador i processos donar i llegir punts

---
+++ src/s1/Jugador.java
@@ -0,0 +1,39 @@
+package s1;
+
+public class Jugador implements Comparable<Jugador> {
+	private String nom;
+	private int punts;
+	
+	public Jugador (String Nom) {
+		this.nom = Nom;
+	}
+
+	public Jugador (String nom, int p) {
+		this.nom = nom;
+		punts = p;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getPunts() {
+		return punts;
+	}
+
+	public void setPunts(int punts) {
+		this.punts += punts;
+	}
+
+	@Override
+	public int compareTo(Jugador o) {
+		 int byPunts = Integer.compare(this.punts, o.getPunts());
+	     return byPunts;
+	}
+	
+
+}
